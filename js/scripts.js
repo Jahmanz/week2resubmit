68a759038949da8c908580ce00f6c70eeb15dac1
Jahmanz Williams <savojah@yahoo.com>
2018-01-19 16:31:09 -0800

    adds correct js back end and front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,41 +7,26 @@ $(document).ready(function() {
     var newoldInput= $("#newold").val();
     var broadInput = $("#broad").val();
 
-    if ((frontbackInput === 'frontend') && (bigsmallInput === "small") && (softwareInput === "enduser") && (newoldInput === "old") && (broadInput === "yes")) {
+    if ((frontbackInput === "frontend") && (bigsmallInup = "small") && (softwareInput = "business") && (newoldInput = "old") && (broadInput = "yes"))
+  {
       $("#clickable").click();
       $(".answerCSS").show("#css");
       $("#formOne").hide();
 
-    //   } if ((frontbackInput === "backend") && (bigsmallInput === "big") && (softwareInput === "business") && (newoldInput === "old") && (broadInput === "yes")) {
-    //   $("#clickable").click();
-    //   $(".answerCsharp").show("#csharp");
-    //   $(".answerJava").show("#java");
-    //   $("#formOne").hide();
-    //
-    //   } if ((frontbackInput === "backend") && (bigsmallInput === "small") && (softwareInput === "enduser") && (newoldInput === "new") && (broadInput === "yes")) {
-    //   $("#clickable").click();
-    //   $(".answerRuby").show("#ruby");
-    //   $("#formOne").hide();
-    //
-    // } if ((frontbackInput === "frontend") && (bigsmallInput === "small") && (softwareInput === "enduser") && (newoldInput === "new") && (broadInput === "yes")) {
-    //   $("#clickable").click();
-    //   $(".answerRuby").show("#ruby");
-    //   $(".answerCSS").show("#css");
-    //   $("#formOne").hide();
-    //
-    // } if ((frontbackInput === "backend") && (bigsmallInput === "big") && (softwareInput === "business") && (newoldInput === "old") && (broadInput === "yes")) {
-    //   $("#clickable").click();
-    //   $(".answerCsharp").show("#csharp");
-    //   $(".answerPhp").show("#php");
-    //   $("#formOne").hide();
+   } if ((frontbackInput === "backend") && (bigsmallInup = "big") && (softwareInput = "business") && (newoldInput = "old") && (broadInput = "no"))
+      {
+        $("#clickable").click();
+        $(".answerCsharp").show("#csharp");
+        $("#formOne").hide();
 
-   // } else {
-   //  $("#clickable").click();
-   //  $(".answerJava").show("#java");
-   //  $(".answerPhp").show("#php");
-   //  $(".answerCsharp").show("#csharp");
-   //  $(".answerRuby").show("#ruby");
-   //  $("#formOne").hide();
+   } else {
+    $("#clickable").click();
+    $(".answerJava").show("#java");
+    $(".answerPhp").show("#php");
+    $(".answerCsharp").show("#csharp");
+    $(".answerRuby").show("#ruby");
+    $("#formOne").hide();
+     alert('Good news, you qualify for all of these courses! Take a look and see which course you want to take.');
   }
   });
 });
